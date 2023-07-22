c0d24e9fae41eb32b83ce99d3332ed02fc15b678
StylishCS <joeshirf@gmail.com>
2023-07-22 10:56:25 +0300

    n

--- db/dbConnection.js
+++ db/dbConnection.js
@@ -1,12 +1,20 @@
 const mysql = require("mysql");
 
 const connection = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "",
-    database: "db",
+    host: "bykoxpxsybzqzwzbmww8-mysql.services.clever-cloud.com",
+    user: "u4ptjd6izxqtlgbq",
+    password: "bgfPeCuyrFUVFI6eYEP7",
+    database: "bykoxpxsybzqzwzbmww8",
     port: "3306",
 });
+
+// const connection = mysql.createConnection({
+//     host: "localhost",
+//     user: "root",
+//     password: "",
+//     database: "db",
+//     port: "3306",
+// });
 
 connection.connect((err) => {
     if (err) throw err;
