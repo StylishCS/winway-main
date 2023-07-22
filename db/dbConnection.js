e838db06e5682c1c051f882e10f9dd911a9cb8bd
StylishCS <joeshirf@gmail.com>
2023-07-22 11:45:04 +0300

    m

--- db/dbConnection.js
+++ db/dbConnection.js
@@ -1,11 +1,19 @@
 const mysql = require("mysql");
 
+const db_config = {
+  host: "bykoxpxsybzqzwzbmww8-mysql.services.clever-cloud.com",
+  user: "u4ptjd6izxqtlgbq",
+  password: "rQq9qDfi7NrQAFtGg1LW",
+  database: "bykoxpxsybzqzwzbmww8",
+  port: "3306",
+};
+
 const connection = mysql.createConnection({
-    host: "bykoxpxsybzqzwzbmww8-mysql.services.clever-cloud.com",
-    user: "u4ptjd6izxqtlgbq",
-    password: "rQq9qDfi7NrQAFtGg1LW",
-    database: "bykoxpxsybzqzwzbmww8",
-    port: "3306",
+  host: "bykoxpxsybzqzwzbmww8-mysql.services.clever-cloud.com",
+  user: "u4ptjd6izxqtlgbq",
+  password: "rQq9qDfi7NrQAFtGg1LW",
+  database: "bykoxpxsybzqzwzbmww8",
+  port: "3306",
 });
 
 // const connection = mysql.createConnection({
@@ -16,9 +24,22 @@ const connection = mysql.createConnection({
 //     port: "3306",
 // });
 
-connection.connect((err) => {
+function handleDisconnect() {
+  connection = mysql.createConnection(db_config);
+  connection.connect((err) => {
     if (err) throw err;
     console.log("DB CONNECTED");
-});
+  });
 
-module.exports = {connection};
+  connection.on("error", function (err) {
+    console.log("db error", err);
+    if (err.code === "PROTOCOL_CONNECTION_LOST") {
+      handleDisconnect();
+    } else {
+      throw err;
+    }
+  });
+}
+
+handleDisconnect();
+module.exports = { connection };
