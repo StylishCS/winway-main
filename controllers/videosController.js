622adc8123d377ef4a1273af2525c8da07f91cc2
Abdelrahman435 <ah0383723@gmail.com>
2023-08-01 17:42:33 +0300

    err

--- controllers/videosController.js
+++ controllers/videosController.js
@@ -111,7 +111,7 @@ async function create(req, res) {
     }
   } catch (err) {
     console.error(err);
-    res.status(500).json({ errors: ["Internal server error"] });
+    res.status(500).json(err);
   }
 }
 
