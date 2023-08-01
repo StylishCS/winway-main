22dd59ced45a7241d2fc85beab5a4218e9dfa1a1
Abdelrahman435 <ah0383723@gmail.com>
2023-08-01 16:40:50 +0300

    protect

--- routes/module.js
+++ routes/module.js
@@ -138,7 +138,7 @@ var router = express.Router();
 const{addModule,getModules ,getModuleOne ,deleteM}= require("../controllers/modulesController")
 const {protect} = require('../middleware/protect');
 
-router.post('/:course_id',addModule)
+router.post('/:course_id',protect,addModule)
 router.get('/:course_id',protect,getModules)
 router.get('/one/:id',protect,getModuleOne)
 router.delete('/del/:id',protect, deleteM)
